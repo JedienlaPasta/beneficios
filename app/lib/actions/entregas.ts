5f419ff54270eea57b965373f7dd57b3b44df07f
Seba <sebastian1377@gmail.com>
2025-03-12 10:42:02 -0300

    qfix 03

--- app/lib/actions/entregas.ts
+++ app/lib/actions/entregas.ts
@@ -1,6 +1,6 @@
-import postgres from "postgres";
+// import postgres from "postgres";
 
-const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
+// const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
 
 // Crear Entrega
 
