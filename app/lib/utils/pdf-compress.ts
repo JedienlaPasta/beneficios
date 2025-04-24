a139394dcd368f40c8618165c8e15ed8398985e4
Seba <sebastian1377@gmail.com>
2025-04-24 15:20:22 -0400

    ghostscript fix on linux qfix01

--- app/lib/utils/pdf-compress.ts
+++ app/lib/utils/pdf-compress.ts
@@ -2,9 +2,7 @@ import { spawn } from "child_process";
 
 export async function compressPdfBuffer(inputBuffer: Buffer): Promise<Buffer> {
   return new Promise((resolve, reject) => {
-    const gsCommand =
-      process.env.GHOSTSCRIPT_COMMAND ||
-      (process.platform === "win32" ? "gswin64c" : "gs");
+    const gsCommand = process.env.GHOSTSCRIPT_COMMAND || "gs";
     const gs = spawn(gsCommand, [
       "-sDEVICE=pdfwrite",
       "-dCompatibilityLevel=1.4",
