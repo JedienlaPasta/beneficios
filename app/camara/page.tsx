ed376cd2d0a9428b5c1597dfeb2aac60df598d5c
Sebastián Ignacio Castaneda Brito <scastaneda@elquisco.cl>
2025-06-14 19:48:01 -0400

    webcam test - modal layout fix

---
+++ app/camara/page.tsx
@@ -0,0 +1,39 @@
+import {
+  fetchEntregasGeneralInfoByFolio,
+  fetchEntregasInfoByFolio,
+  fetchFilesByFolio,
+} from "@/app/lib/data/entregas";
+import ModalEntregasDetail from "@/app/ui/dashboard/entregas/[id]/modal-entregas-detail";
+import React, { Suspense } from "react";
+import { Modal } from "../ui/dashboard/modal";
+import ModalSkeleton from "../ui/modal-skeleton";
+
+export default async function Test() {
+  const folio = "1-25-TA";
+  const rut = "12345678";
+  const [entregasResponse, entregaResponse, filesResponse] = await Promise.all([
+    fetchEntregasGeneralInfoByFolio(folio),
+    fetchEntregasInfoByFolio(folio),
+    fetchFilesByFolio(folio),
+  ]);
+
+  const entregas = entregasResponse;
+  const entrega = entregaResponse;
+  const files = filesResponse;
+
+  return (
+    <div className="flex h-screen w-full flex-col gap-6">
+      <Modal name="detailsModal" secondName="rut">
+        <Suspense fallback={<ModalSkeleton name="detailsModal" />}>
+          <ModalEntregasDetail
+            rut={rut}
+            folio={folio}
+            entregas={entregas}
+            entrega={entrega}
+            files={files}
+          />
+        </Suspense>
+      </Modal>
+    </div>
+  );
+}
